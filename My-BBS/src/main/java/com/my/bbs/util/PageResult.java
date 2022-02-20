a150055e5a915e2ca0d3b747e18d1a8ee317936d
xrz <1274326376@qq.com>
2022-02-20 11:46:52 +0800

    细节优化

--- My-BBS/src/main/java/com/my/bbs/util/PageResult.java
+++ My-BBS/src/main/java/com/my/bbs/util/PageResult.java
@@ -5,11 +5,6 @@ import java.util.List;
 
 /**
  * 分页工具类
- *
- * @author 13
- * @qq交流群 719099151
- * @email 2449207463@qq.com
- * @link https://github.com/ZHENFENG13/My-BBS
  */
 public class PageResult implements Serializable {
 
